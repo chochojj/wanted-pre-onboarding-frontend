473b63a61fa79d36ff5b0b5c7b4139a5a8d0bf43
chochojj <csgy114@gmail.com>
2023-08-09 17:12:18 +0900

    fix : 마운트 오류 해결

--- src/apis/api.js
+++ src/apis/api.js
@@ -7,13 +7,19 @@ const axiosApi = axios.create({
   },
 });
 
-const authorizedApi = axios.create({
-  baseURL: "https://www.pre-onboarding-selection-task.shop",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
-});
+const getAuthorizedApi = () => {
+  const token = localStorage.getItem("token");
+
+  const authorizedApi = axios.create({
+    baseURL: "https://www.pre-onboarding-selection-task.shop",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return authorizedApi;
+};
 
 //회원가입
 export const signupApi = async (email, password) => {
@@ -38,6 +44,7 @@ export const signinApi = async (email, password) => {
 //투두작성
 export const createTodoApi = async (todo) => {
   try {
+    const authorizedApi = getAuthorizedApi();
     const response = await authorizedApi.post("/todos", { todo });
     return response.data;
   } catch (error) {
@@ -48,6 +55,7 @@ export const createTodoApi = async (todo) => {
 //투두조회
 export const getTodosApi = async () => {
   try {
+    const authorizedApi = getAuthorizedApi();
     const response = await authorizedApi.get("/todos");
     return response.data;
   } catch (error) {
@@ -58,6 +66,7 @@ export const getTodosApi = async () => {
 //투두수정
 export const updateTodoApi = async (id, todo, isCompleted) => {
   try {
+    const authorizedApi = getAuthorizedApi();
     const response = await authorizedApi.put(`/todos/${id}`, {
       todo,
       isCompleted,
@@ -71,6 +80,7 @@ export const updateTodoApi = async (id, todo, isCompleted) => {
 //투두삭제
 export const deleteTodoApi = async (id) => {
   try {
+    const authorizedApi = getAuthorizedApi();
     const response = await authorizedApi.delete(`/todos/${id}`);
     return response.data;
   } catch (error) {
